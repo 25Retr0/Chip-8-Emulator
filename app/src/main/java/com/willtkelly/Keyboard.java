ec245675f8338b7682c541685ac5462c5c00391b
25Retr0 <06willtk25@gmail.com>
2026-01-21 17:10:54 +1000

    Updated keyboard to ignore unwanted keys.

--- app/src/main/java/com/willtkelly/Keyboard.java
+++ app/src/main/java/com/willtkelly/Keyboard.java
@@ -13,25 +13,25 @@ public class Keyboard implements KeyListener {
         this.keymap = new int[256];
         Arrays.fill(this.keymap, -1);
 
-        this.keymap['1'] = 0x1;
-        this.keymap['2'] = 0x2;
-        this.keymap['3'] = 0x3;
-        this.keymap['4'] = 0xC;
+        this.keymap[KeyEvent.VK_1] = 0x1;
+        this.keymap[KeyEvent.VK_2] = 0x2;
+        this.keymap[KeyEvent.VK_3] = 0x3;
+        this.keymap[KeyEvent.VK_4] = 0xC;
 
-        this.keymap['q'] = 0x4;
-        this.keymap['w'] = 0x5;
-        this.keymap['e'] = 0x6;
-        this.keymap['r'] = 0xD;
+        this.keymap[KeyEvent.VK_Q] = 0x4;
+        this.keymap[KeyEvent.VK_W] = 0x5;
+        this.keymap[KeyEvent.VK_E] = 0x6;
+        this.keymap[KeyEvent.VK_R] = 0xD;
 
-        this.keymap['a'] = 0x7;
-        this.keymap['s'] = 0x8;
-        this.keymap['d'] = 0x9;
-        this.keymap['f'] = 0xE;
+        this.keymap[KeyEvent.VK_A] = 0x7;
+        this.keymap[KeyEvent.VK_S] = 0x8;
+        this.keymap[KeyEvent.VK_D] = 0x9;
+        this.keymap[KeyEvent.VK_F] = 0xE;
 
-        this.keymap['z'] = 0xA;
-        this.keymap['x'] = 0x0;
-        this.keymap['c'] = 0xB;
-        this.keymap['v'] = 0xF;
+        this.keymap[KeyEvent.VK_Z] = 0xA;
+        this.keymap[KeyEvent.VK_X] = 0x0;
+        this.keymap[KeyEvent.VK_C] = 0xB;
+        this.keymap[KeyEvent.VK_V] = 0xF;
 
 
         this.keyState = new boolean[16];
@@ -46,23 +46,38 @@ public class Keyboard implements KeyListener {
         return this.keyState[key];
     }
 
+    public boolean[] getKeysPressed() {
+        return Arrays.copyOf(keyState, keyState.length);
+    }
+
+
     @Override
     public void keyTyped(KeyEvent keyEvent) {
         // Unused for Chip-8 Emulators
     }
 
     @Override
-    public void keyPressed(KeyEvent keyEvent) {
-        // Set the pressed key to true in keyState
-        int key = keyEvent.getKeyCode();
-        this.keyState[key] = true;
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code < 0 || code >= keymap.length) return;
+
+        int chip8Key = keymap[code];
+        if (chip8Key != -1) {
+            keyState[chip8Key] = true;
+        }
     }
 
     @Override
-    public void keyReleased(KeyEvent keyEvent) {
-        // Set the pressed key to false in the keyState
-        int key = keyEvent.getKeyCode();
-        this.keyState[key] = false;
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code < 0 || code >= keymap.length) return;
+
+        int chip8Key = keymap[code];
+        if (chip8Key != -1) {
+            keyState[chip8Key] = false;
+        }
     }
 }
 
